ba8c7663462155b2f196080a75f9070b649cbd2c
Hyuk kim <rlagur0222@gmail.com>
2022-01-24 16:28:54 +0900

    가위바위보게임

---
+++ test1.py
@@ -0,0 +1,30 @@
+import random
+a=random.randint(1,4)
+b=input("1~3까지의 숫자를 입력하세요(1:바위, 2:가위, 3:보):")
+
+if a==1:
+    print("컴퓨터가 바위를 냈습니다.")
+    if b==1:
+        print("비겼습니다.")
+    elif b==2:
+        print("졌습니다.")
+    else:
+        print("이겼습니다.")
+
+elif a==2:
+    print("컴퓨터가 가위를 냈습니다.")
+    if b==1:
+        print("이겼습니다.")
+    elif b==2:
+        print("비겼습니다.")
+    else:
+        print("졌습니다.")
+
+elif a==3:
+    print("컴퓨터가 보를 냈습니다.")
+    if b == 1:
+      print("졌습니다.")
+    elif b == 2:
+        print("이겼습니다.")
+    else:
+        print("비겼습니다.")
